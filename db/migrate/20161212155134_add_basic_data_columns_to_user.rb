ed1ff2d3b4190536a8a33f5978b45fa7761cc68f
Will Tinney <willtinney1@gmail.com>
2016-12-12 15:59:05 +0000

    First user database migration

---
+++ db/migrate/20161212155134_add_basic_data_columns_to_user.rb
@@ -0,0 +1,16 @@
+class AddBasicDataColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :email, :string
+    add_column :users, :password, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address_line_1, :string
+    add_column :users, :address_line_2, :string
+    add_column :users, :town, :string
+    add_column :users, :country, :string
+    add_column :users, :postcode, :string
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+    add_column :users, :profile_picture, :string
+  end
+end
