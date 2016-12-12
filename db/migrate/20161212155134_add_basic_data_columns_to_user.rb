3666db4f2f7969dbdf50655b07775d4ef75fdda2
Will Tinney <willtinney1@gmail.com>
2016-12-12 17:39:15 +0000

    Tidied up devise sign-in/sign-up

--- db/migrate/20161212155134_add_basic_data_columns_to_user.rb
+++ db/migrate/20161212155134_add_basic_data_columns_to_user.rb
@@ -1,7 +1,5 @@
 class AddBasicDataColumnsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :email, :string
-    add_column :users, :password, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :address_line_1, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211104129) do
+ActiveRecord::Schema.define(version: 20161212155134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20161211104129) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address_line_1"
+    t.string   "address_line_2"
+    t.string   "town"
+    t.string   "country"
+    t.string   "postcode"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
