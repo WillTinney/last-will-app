b2d0db182885b3a021edc2e76dd2a2efbbab5819
Will Tinney <willtinney1@gmail.com>
2016-12-15 09:03:44 +0000

    Configured devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource_or_scope)
+    current_user
+  end
+
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
 end
