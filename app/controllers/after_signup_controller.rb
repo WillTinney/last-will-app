9185ee5fcaed2e96334508d9a57f2c7db4c68d55
Will Tinney <willtinney1@gmail.com>
2016-12-16 14:41:37 +0000

    Created after_signup controller with wicked

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -1,2 +1,17 @@
-class AfterSignUpController < ApplicationController
+class AfterSignupController < ApplicationController
+  include Wicked::Wizard
+
+  steps :sign_up, :basic_profile, :advanced_profile
+
+  def show
+    @user = current_user
+    sign_in(@user, bypass: true)
+    render_wizard
+  end
+
+  def update
+    @user = current_user
+    @user.update_attributes(params[:user])
+    render_wizard @user
+  end
 end
