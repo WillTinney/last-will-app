f2cc06e7e9055742f207c9dd02b141f476968c58
Will Tinney <willtinney1@gmail.com>
2017-01-01 14:11:45 +0000

    Added guardian routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'guardians/index'
-
-  get 'guardians/new'
-
-  get 'guardians/create'
-
-  get 'guardians/show'
-
-  get 'guardians/edit'
-
-  get 'guardians/update'
-
-  get 'guardians/destroy'
-
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root to: 'pages#home'
@@ -22,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [ :show, :edit, :update ] do
     # Routes for wizard sign-up
     resources :after_sign_up
+    resources :guardians
   end
 
 end
