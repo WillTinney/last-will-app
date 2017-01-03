7f071f1bed4bb8a23d44e42df61462f2b3b09889
Will Tinney <willtinney1@gmail.com>
2017-01-03 13:31:22 +0000

    Added routes for digital asset pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,38 @@ Rails.application.routes.draw do
   # Routes for core user side
   resources :users, only: [ :show, :edit, :update ] do
 
-    resources :guardians
-    resources :approvers
-    resources :recipients
-
-    # Routes for wizard sign-up
-    resources :after_sign_up
-
     # User profile page
     get 'profile', to: 'users#profile'
 
     # User call-to-action page
     get 'call_to_action', to: 'users#call_to_action'
+
+    # Guardian Pages
+    resources :guardians do
+      get 'notes', to: 'guardians#notes'
+      get 'admin', to: 'guardians#admin'
+      get 'photos', to: 'guardians#photos'
+      get 'videos', to: 'guardians#videos'
+    end
+
+    # Approver Pages
+    resources :approvers do
+      get 'notes', to: 'approvers#notes'
+      get 'admin', to: 'approvers#admin'
+      get 'photos', to: 'approvers#photos'
+      get 'videos', to: 'approvers#videos'
+    end
+
+    # Recipient Pages
+    resources :recipients do
+      get 'notes', to: 'recipients#notes'
+      get 'admin', to: 'recipients#admin'
+      get 'photos', to: 'recipients#photos'
+      get 'videos', to: 'recipients#videos'
+    end
+
+    # Routes for wizard sign-up
+    resources :after_sign_up
 
   end
 
