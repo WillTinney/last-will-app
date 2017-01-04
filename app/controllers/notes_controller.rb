f9a514511533c061a6b060be683beec42d695d1c
Will Tinney <willtinney1@gmail.com>
2017-01-04 14:04:54 +0000

    Configured notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,42 @@
 class NotesController < ApplicationController
+   before_action :set_note, only: [:edit, :update, :destroy]
+
   def new
+    @note = Note.new
   end
 
   def create
+    @note = current_user.guardians.build(guardian_params)
+    if @note.save
+      redirect_to user_path(current_user), notice: 'Note was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @note.update(guardian_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
   def destroy
+    @note.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def user_params
+    params.require(:note).permit(:title, :content)
   end
 end
