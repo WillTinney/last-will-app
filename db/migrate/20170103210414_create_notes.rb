d189d933ddbfedfd942d26110d8c2516d3218d13
Will Tinney <willtinney1@gmail.com>
2017-01-04 17:47:27 +0000

    Created notes new page

--- db/migrate/20170103210414_create_notes.rb
+++ db/migrate/20170103210414_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
       t.string :title
-      t.string :caption
+      t.string :content
       t.references :approver, foreign_key: true
       t.references :guardian, foreign_key: true
       t.references :recipient, foreign_key: true
