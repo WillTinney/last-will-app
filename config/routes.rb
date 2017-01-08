fb4aab8415f05584b8f2e47c945f00f4310d67f5
Will Tinney <willtinney1@gmail.com>
2017-01-08 11:17:29 +0000

    Created route for user notes page

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,9 @@ Rails.application.routes.draw do
     # User proof of residency page
     get 'proof', to: 'users#proof'
 
+    # User notes page
+    get 'notes', to: 'user#notes'
+
     # User admin references page
     get 'admin', to: 'users#admin'
 
