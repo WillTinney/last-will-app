d08f61933f33cfd64a50335a17b6f5a45e7047a4
Will Tinney <willtinney1@gmail.com>
2017-01-10 22:38:15 +0000

    Added logic to videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,22 +1,49 @@
 class VideosController < ApplicationController
+  before_action :set_video, only: [:edit, :update, :destroy]
+
   def index
+    @videos = Video.all
   end
 
   def show
   end
 
   def new
+    @video = Video.new
   end
 
   def create
+    @video = current_user.videos.build(video_params)
+    if @video.save
+      redirect_to user_path(current_user), notice: 'Video was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @video.update(video_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @video.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def set_video
+    @video = video.find(params[:id])
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :description, :video)
   end
 end
