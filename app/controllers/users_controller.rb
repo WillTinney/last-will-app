0f4aadef726af89136e73242fc0a6b461a3d28e5
Will Tinney <willtinney1@gmail.com>
2017-01-10 11:58:26 +0000

    Added attachinary logic to controller + model for proof£

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name,
       :address_line_1, :address_line_2, :town, :country, :postcode,
-      :latitude, :longitude, :profile_picture)
+      :latitude, :longitude, :profile_picture, :proof_of_residency,
+      :proof_comments)
   end
 end
