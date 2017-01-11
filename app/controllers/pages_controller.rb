77e5e881720eadb9946468b56a55cb6c214ae59e
Christos Dimitroulas <c.dimitroulas@gmail.com>
2017-01-11 13:08:47 +0000

    success notice appears when subscribing to mailchimp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,28 @@
+require 'pry'
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :subscribe ]
 
   def home
   end
 
   def subscribe
-    binding.pry
-    gibbon = Gibbon::Request.new
-    # gibbon.lists(list_id).members.create(body:
-    #   {email_address: "foo@bar.com",
-    #     status: "subscribed"})
+
+    subscribe_user_to_mailchimp
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
 
-  # def subscriber_params
-  #   params.require(:).permit(:first_
-  # end
+  def subscriber_params
+    params.require(:subscriber).permit(:email)
+  end
+
+  def subscribe_user_to_mailchimp
+    gibbon = Gibbon::Request.new
+    gibbon.lists(ENV['MAIL_CHIMP_LIST_ID']).members.create(body:
+      {email_address: subscriber_params[:email],
+      status: "subscribed"})
+  end
 end
