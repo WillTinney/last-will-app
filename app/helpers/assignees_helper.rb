f10ce441ac0de75141c45bc0a39edcd793bf52cf
Will Tinney <willtinney1@gmail.com>
2017-01-11 14:38:56 +0000

    Added assignee path helpers

--- app/helpers/assignees_helper.rb
+++ app/helpers/assignees_helper.rb
@@ -1,2 +1,16 @@
 module AssigneesHelper
+
+  # Returns a dynamic path based on the provided parameters
+  def sti_assignee_path(type = "assignee", assignee = nil, action = nil)
+    send "#{format_sti(action, type, assignee)}_path", assignee
+  end
+
+  def format_sti(action, type, assignee)
+    action || assignee ? "#{format_action(action)}#{type.underscore}" : "#{type.underscore.pluralize}"
+  end
+
+  def format_action(action)
+    action ? "#{action}_" : ""
+  end
+
 end
