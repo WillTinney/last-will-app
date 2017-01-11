eca278187ea5ed5a8e1465a31d94ee6dd1301d07
Will Tinney <willtinney1@gmail.com>
2017-01-11 14:31:10 +0000

    Added basic controller logic to assignees controller

--- app/controllers/assignees_controller.rb
+++ app/controllers/assignees_controller.rb
@@ -1,22 +1,69 @@
 class AssigneesController < ApplicationController
+  before_action :set_assignee, only: [:show, :edit, :update, :destroy]
+  before_action :set_assignee_id, only: [:notes, :admin, :photos, :videos]
+
   def index
+    @approvers = Assignee.all
   end
 
   def show
+    @user = User.where('id = ?', params[:user_id]).first
   end
 
   def new
+    @assignee = Assignee.new
   end
 
   def create
+    @assignee = current_user.approvers.build(approver_params)
+    if @assignee.save
+      redirect_to user_path(current_user), notice: 'Assignee was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+   if @assignee.update(approver_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @assignee.destroy
+    redirect_to user_path(current_user)
+  end
+
+  def notes
+  end
+
+  def admin
+  end
+
+  def photos
+  end
+
+  def videos
+  end
+
+  private
+
+  def set_assignee
+    @assignee = Assignee.find(params[:id])
+  end
+
+  def set_assignee_id
+    @assignee = Assignee.find(params[:assignee_id])
+  end
+
+  def approver_params
+    params.require(:assignee).permit(:first_name, :middle_name, :last_name, :citizenship,
+      :date_of_birth, :email, :phone_number, :address_line_1, :address_line_2,
+      :town, :country, :postcode, :relationship, :profile_picture)
   end
 end
