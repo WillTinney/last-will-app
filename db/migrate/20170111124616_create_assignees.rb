a621806554d148e4f94f34b087c542467ef031ae
Will Tinney <willtinney1@gmail.com>
2017-01-11 12:48:10 +0000

    Created assignee model

---
+++ db/migrate/20170111124616_create_assignees.rb
@@ -0,0 +1,23 @@
+class CreateAssignees < ActiveRecord::Migration[5.0]
+  def change
+    create_table :assignees do |t|
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :relationship
+      t.string :citizenship
+      t.string :date_of_birth
+      t.string :email
+      t.string :phone_number
+      t.string :address_line_1
+      t.string :address_line_2
+      t.string :town
+      t.string :country
+      t.string :postcode
+      t.string :profile_picture
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
