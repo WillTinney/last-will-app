b891ee688e563302db7e4b36234beef9b299be16
Will Tinney <willtinney1@gmail.com>
2017-01-11 15:36:48 +0000

    Created assignee sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'assignees/index'
-
-  get 'assignees/show'
-
-  get 'assignees/new'
-
-  get 'assignees/create'
-
-  get 'assignees/edit'
-
-  get 'assignees/update'
-
-  get 'assignees/destroy'
-
-  get 'videos/index'
-
-  get 'videos/show'
-
-  get 'videos/new'
-
-  get 'videos/create'
-
-  get 'videos/edit'
-
-  get 'videos/update'
-
-  get 'videos/destroy'
-
   mount Attachinary::Engine => "/attachinary"
 
   devise_for :users, controllers: { registrations: 'registrations' }
@@ -37,63 +9,32 @@ Rails.application.routes.draw do
   # Routes for core user side
   resources :users, only: [ :show, :edit, :update ] do
 
-    # User profile page
+    # User Pages
     get 'profile', to: 'users#profile'
-
-    # User call-to-action page
     get 'action', to: 'users#action'
-
-    # User digital assets overview page
     get 'digital', to: 'users#digital'
-
-    # User proof of residency page
     get 'proof', to: 'users#proof'
-
-    # User notes page
     get 'notes', to: 'users#notes'
-
-    # User admin references page
     get 'admin', to: 'users#admin'
-
-    # User photo library page
     get 'photos', to: 'users#photos'
-
-    # User video library page
     get 'videos', to: 'users#videos'
 
-    # Guardian Pages
-    resources :guardians do
-      get 'notes', to: 'guardians#notes'
-      get 'admin', to: 'guardians#admin'
-      get 'photos', to: 'guardians#photos'
-      get 'videos', to: 'guardians#videos'
+    # Assignee Pages
+    resources :assignees do
+      get 'notes', to: 'assignees#notes'
+      get 'admin', to: 'assignees#admin'
+      get 'photos', to: 'assignees#photos'
+      get 'videos', to: 'assignees#videos'
       resources :notes, only: [:new, :create, :edit, :update, :destroy]
       resources :photos, only: [:new, :create, :edit, :update, :destroy]
       resources :references, only: [:new, :create, :edit, :update, :destroy]
     end
-
-    # Approver Pages
-    resources :approvers do
-      get 'notes', to: 'approvers#notes'
-      get 'admin', to: 'approvers#admin'
-      get 'photos', to: 'approvers#photos'
-      get 'videos', to: 'approvers#videos'
-      resources :notes, only: [:new, :create, :edit, :update, :destroy]
-      resources :photos, only: [:new, :create, :edit, :update, :destroy]
-      resources :references, only: [:new, :create, :edit, :update, :destroy]
+    resources :approvers, controller: 'assignees', type: 'Approver' do
+      get 'notes', to: 'assignees#notes'
+      get 'admin', to: 'assignees#admin'
+      get 'photos', to: 'assignees#photos'
+      get 'videos', to: 'assignees#videos'
     end
-
-    # Recipient Pages
-    resources :recipients do
-      get 'notes', to: 'recipients#notes'
-      get 'admin', to: 'recipients#admin'
-      get 'photos', to: 'recipients#photos'
-      get 'videos', to: 'recipients#videos'
-      resources :notes, only: [:new, :create, :edit, :update, :destroy]
-      resources :photos, only: [:new, :create, :edit, :update, :destroy]
-      resources :references, only: [:new, :create, :edit, :update, :destroy]
-    end
-
     # Routes for wizard sign-up
     resources :after_sign_up
 
