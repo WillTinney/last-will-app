0dd5d5706e90720715fe2ad2c50d17ec53872b1d
Will Tinney <willtinney1@gmail.com>
2017-01-13 09:05:01 +0000

    Changed spelling mistake in assignees controller

--- app/controllers/assignees_controller.rb
+++ app/controllers/assignees_controller.rb
@@ -66,8 +66,12 @@ class AssigneesController < ApplicationController
     @type = params[:type]
   end
 
+  def type
+    params[:type]
+  end
+
   def assignee_params
-    params.require(:assignee).permit(:first_name, :middle_name, :last_name, :citizenship,
+    params.require(type.underscore.to_sym).permit(:first_name, :middle_name, :last_name, :citizenship,
       :date_of_birth, :email, :phone_number, :address_line_1, :address_line_2,
       :town, :country, :postcode, :relationship, :profile_picture, :type)
   end
