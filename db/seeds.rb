7fd1fbc010615cf5b20b7569069ac0ea6b15fbcd
Will Tinney <willtinney1@gmail.com>
2017-02-27 17:54:39 +0000

    Added Call to Action

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 mobile_string = '07' + ('%09d' % rand(10 ** 9)).to_s
 
 User.create!(title: "Mr", email: "test@gmail.com", password: "123123", first_name: "John", last_name: "Hartaway",
